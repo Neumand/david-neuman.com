6753feeb325d8e5575690ff25533ab9f70e35357
David Neuman <david.m.neuman@gmail.com>
2021-01-28 16:36:56 -0500

    Some style updates + date-fns

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,14 +1,30 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { format, parseISO } from "date-fns";
 import { getAllPosts, getPost } from "../../server/ghost";
 
 const PostPage = ({ post }) => {
-  console.log(post);
   return (
-    <Flex direction="column" justifyContent="center" alignItems="center" padding="30px">
+    <Flex
+      as="main"
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      padding="30px"
+      margin="0 auto"
+      maxWidth="720px"
+      w="100%">
       <Heading>{post.title}</Heading>
-      <Text>{`Reading time: ${post.reading_time} ${
-        post.reading_time > 1 ? "minutes" : "minute"
-      }`}</Text>
+      <Box display="flex" justifyContent="space-between" w="100%">
+        <Flex px={8}>
+          <Text mr={8} fontSize="0.9rem">
+            David Neuman
+          </Text>
+          <Text fontSize="0.9rem">{format(parseISO(post.created_at), "MMMM do, yyyy")}</Text>
+        </Flex>
+        <Text fontSize="0.9rem">{`Reading time: ${post.reading_time} ${
+          post.reading_time > 1 ? "minutes" : "minute"
+        }`}</Text>
+      </Box>
       <Box dangerouslySetInnerHTML={{ __html: post.html }}></Box>
     </Flex>
   );
