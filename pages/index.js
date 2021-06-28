2b2ad71c1a01f070e00bcadba5ff55a8818ea8f2
David Neuman <dneuman@stingray.com>
2021-06-28 15:30:19 -0400

    Implement typed.js on home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
+import { useEffect, useRef } from 'react';
 import Image from 'next/image';
+import Typed from 'typed.js';
 
 import Layout from 'layouts/Layout';
+import Header from 'components/Header';
 import Tags from 'components/Tags';
 import { getFeaturedPosts } from 'server/ghost';
 
 const Home = ({ featuredPosts }) => {
+  const typedEl = useRef(null);
+  const typed = useRef(null);
+
+  useEffect(() => {
+    typed.current = new Typed(typedEl.current, {
+      strings: ['writes code.', 'writes music.', 'writes words on a screen.'],
+      typeSpeed: 35,
+      backSpeed: 35,
+      backDelay: 2000,
+      loop: true,
+    });
+
+    return () => typed.current.destroy();
+  });
+
   return (
-    <Layout
-      header="David Neuman"
-      subHeader="Full-stack software developer, musician, and writer."
-    >
+    <Layout>
+      <Header textAlign="left">
+        <h1 className="text-6xl font-bold">David Neuman</h1>
+        <span className="text-3xl font-semibold text-blue-800" ref={typedEl}></span>
+      </Header>
       <h2 className="text-xl uppercase font-semibold">Featured Posts</h2>
       {featuredPosts.map((post) => (
         <div key={post.id}>
