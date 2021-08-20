064634717b76f5062b9cbafed04234509e8495cc
neumand <david.m.neuman@gmail.com>
2021-08-20 14:58:07 -0400

    Destructure book stuff

--- pages/books/index.js
+++ pages/books/index.js
@@ -11,23 +11,23 @@ export default function Books({ books }) {
       </Header>
       <div className="container p-5 my-10 mx-auto">
         <div className="grid gap-4 mb-8 mx-auto md:grid-cols-2 md:gap-8 xl:grid-cols-3">
-          {books.map((book) => (
+          {books.map(({ title, author, imageUrl, rating, summary, genre }) => (
             <div
-              key={book.title}
+              key={title}
               className="flex p-4 max-h-72 max-w-xl bg-gray-50 border-2 rounded-lg shadow-sm transition-all transform ease-in-out md:hover:scale-105 md:hover:shadow-xl md:active:scale-105 md:active:shadow-xl dark:bg-cool-gray-900 dark:border-cool-gray-700"
             >
               <img
-                src={book.imageUrl}
-                alt={`Book cover for ${book.title} by ${book.author}`}
+                src={imageUrl}
+                alt={`Book cover for ${title} by ${author}`}
                 className="w-30 max-h-52 md:w-44 md:max-h-64 rounded-md shadow-md"
               />
               <div className="flex flex-col max-w-[150px] m-auto text-center space-y-2 md:max-w-[200px]">
-                <h3 className="text-base md:text-xl font-bold">{book.title}</h3>
-                <div className="text-sm">{book.author}</div>
-                {book.rating && <p className="text-xs md:text-sm">{book.rating}</p>}
-                <p className="text-left text-xs md:text-sm">{book.summary}</p>
+                <h3 className="text-base md:text-xl font-bold">{title}</h3>
+                <div className="text-sm">{author}</div>
+                {rating && <p className="text-xs md:text-sm">{rating}</p>}
+                <p className="text-left text-xs md:text-sm">{summary}</p>
                 <div className="text-xs mt-auto font-semibold uppercase text-blue-900 dark:text-blue-500">
-                  {book.genre}
+                  {genre}
                 </div>
               </div>
             </div>
