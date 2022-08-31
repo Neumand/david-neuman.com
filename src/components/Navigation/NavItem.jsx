791f64bb0343b9a5362c4dac7b3fee931575212e
David Neuman <david.m.neuman@gmail.com>
2022-08-31 12:16:51 -0400

    Move import of global.css to BaseLayout

--- src/components/Navigation/NavItem.jsx
+++ src/components/Navigation/NavItem.jsx
@@ -1,5 +1,3 @@
-import '../../styles/globals.css';
-
 export default function NavItem({ children, href = null, popOut }) {
   if (!href) {
     return <span className='cursor-pointer hover:font-bold'>{children}</span>;
