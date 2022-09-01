8fa8b2cf154dc015fbddf6bb61a5a26ddf248600
David Neuman <david.m.neuman@gmail.com>
2022-09-01 15:11:02 -0400

    Remove tailwind base styles on every page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), preact()],
+  integrations: [
+    tailwind({ config: { applyBaseStyles: false } }),
+    image(),
+    preact(),
+  ],
 });
