bc63aed255ddd259c294079fd8c23a93ec8b1531
David Neuman <david.m.neuman@gmail.com>
2023-01-10 14:40:22 -0500

    Remove all newsletter resources + link to Substack Remove useless draft issues

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { name: 'Blog', href: '/blog', popOut: false },
-  { name: 'Newsletter', href: '/newsletter', popOut: false },
+  { name: 'Newsletter', href: 'https://davidneuman.substack.com/', popOut: true },
   { name: 'Books', href: '/books', popOut: false },
   { name: 'Now', href: '/now', popOut: false },
   { name: 'About', href: '/about', popOut: false },
