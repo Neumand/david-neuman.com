1ac49fba1bbcd7c43f6b721f3c14b64be0c1e3ec
David Neuman <david.m.neuman@gmail.com>
2024-01-12 08:04:38 -0500

    Add floating navbar

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,11 @@
 export const navItems = [
-  { name: 'Blog', href: '/blog', popOut: false },
-  { name: 'Newsletter', href: 'https://davidneuman.substack.com/', popOut: true },
+  { name: 'Writing', href: '/blog', popOut: false },
+  {
+    name: 'Newsletter',
+    href: 'https://www.bit-by-bit.co/',
+    popOut: true,
+  },
   { name: 'Books', href: '/books', popOut: false },
   { name: 'Now', href: '/now', popOut: false },
   { name: 'About', href: '/about', popOut: false },
-  {
-    name: 'Brain Food',
-    href: 'https://brain-food.vercel.app/',
-    popOut: true,
-  },
 ];
