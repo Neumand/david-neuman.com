a5ed3a7c0ae4d2b3a603ee463cf7231fadbb864c
David Neuman <david.m.neuman@gmail.com>
2024-01-15 13:57:26 -0500

    Update home page design Add floating nav bar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,34 +2,35 @@ import { motion } from 'framer-motion';
 
 export const Hero = () => {
   return (
-    <div className='flex flex-col p-8 gap-10 h-[25rem] w-full dark:bg-cool-gray-900 bg-gray-200  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative items-center justify-center'>
+    <div className='flex flex-col p-8 gap-10 h-[25rem] w-full dark:bg-cool-gray-900 bg-gray-200/75  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative items-center justify-center'>
       {/* Radial gradient for the container to give a faded look */}
-      <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
+      <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-zinc-950 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
       <motion.h1
         initial={{ y: -10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.2 }}
         className='text-gray-900 dark:text-gray-300 text-4xl sm:text-7xl font-bold relative'
       >
-        David Neuman
+        Hi, I'm David
       </motion.h1>
 
       <motion.div
         initial={{ y: -10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.4 }}
-        className='flex flex-col gap-2 text-md md:text-lg font- max-w-xl text-center text-gray-800 dark:text-gray-400'
+        className='flex flex-col gap-2 text-md md:text-lg font- max-w-lg text-center text-gray-700 dark:text-gray-500'
       >
         <p>
-          Product-minded engineer learning design. Building digital products
-          users love, and sharing what I learn along the way
+          I'm a product-minded engineer learning{' '}
+          {/* <span className='border border-slate-950 dark:border-gray-700 border-solid px-2 py-2 relative'> */}
+          <span>design.</span>
+          {/* <div className='absolute top-[-4px] -left-[4px] p-0 bg-zinc-700 dark:bg-zinc-50 w-2 aspect-square rounded-full ' />
+            <div className='absolute bottom-[-4px] -left-[4px] p-0 bg-zinc-700 dark:bg-zinc-50 w-2 aspect-square rounded-full ' />
+            <div className='absolute top-[-4px] -right-[4px] p-0 bg-zinc-700 dark:bg-zinc-50 w-2 aspect-square rounded-full ' />
+            <div className='absolute bottom-[-4px] -right-[4px] p-0 bg-zinc-700 dark:bg-zinc-50 w-2 aspect-square rounded-full ' /> */}
+          {/* </span> */}
         </p>
-        <p>
-          Engineering @{' '}
-          <a className='link' href='https://lifehousehotels.com'>
-            Life House
-          </a>
-        </p>
+        <p>Building for the web and sharing what I learn along the way.</p>
       </motion.div>
     </div>
   );
--- src/components/Navigation/FloatingNavbar.tsx
+++ src/components/Navigation/FloatingNavbar.tsx
@@ -14,7 +14,7 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     href: string;
-    icon?: JSX.Element;
+    popOut?: boolean;
   }[];
   className?: string;
 }) => {
@@ -46,7 +46,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          'flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-slate-900 bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] py-2 px-4 items-center justify-center space-x-4',
+          'flex w-[80%] h-15 md:max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-zinc-900 bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] py-2 px-4 items-center justify-center space-x-4',
           className,
         )}
       >
@@ -54,11 +54,12 @@ export const FloatingNav = ({
           <a
             key={`link=${idx}`}
             href={navItem.href}
+            target={navItem.popOut ? '_blank' : '_self'}
             className={cn(
               'relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500',
             )}
           >
-            <span className='hidden sm:block text-sm'>{navItem.name}</span>
+            <span className='text-sm md:text-lg'>{navItem.name}</span>
           </a>
         ))}
       </motion.div>
