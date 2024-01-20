b0b582115885db619d376630fc951dcfba396306
David Neuman <david.m.neuman@gmail.com>
2024-01-20 15:18:21 -0500

    Style adjustments

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,11 +18,11 @@ export const Hero = () => {
         initial={{ y: -10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.4 }}
-        className='flex flex-col gap-2 text-md md:text-lg font- max-w-lg text-center text-gray-700 dark:text-gray-500'
+        className='flex flex-col gap-2 text-md md:text-lg font- max-w-lg text-center text-gray-700 dark:text-gray-400'
       >
         <p>
           I'm a product-minded engineer learning{' '}
-          <span className='border border-slate-950 dark:border-gray-700 border-solid px-2 py-2 relative'>
+          <span className='border border-zinc-950 dark:border-gray-700 border-solid px-2 py-2 relative'>
             <span>design.</span>
             <div className='absolute top-[-4px] -left-[4px] p-0 bg-zinc-700 dark:bg-zinc-50 w-2 aspect-square rounded-full ' />
             <div className='absolute bottom-[-4px] -left-[4px] p-0 bg-zinc-700 dark:bg-zinc-50 w-2 aspect-square rounded-full ' />
